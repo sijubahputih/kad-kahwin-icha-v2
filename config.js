f905397ae8ddd2c668926832c6277cfc089cde2b
sijubahputih <kimie.ummah@gmail.com>
2026-08-26 02:44:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,83 +1,23 @@
 const wedding = {
-  // PENGANTIN
   groom: {
-    first: "NOR ALIF HAIQAL",
-    last: "BIN MOHD NAZRI"
+    fullName: "NOR ALIF HAIQAL BIN MOHD NAZRI",
+    shortName: "Nor Alif Haiqal"
   },
 
   bride: {
-    first: "ARIESYAA ARYANA",
-    last: "BINTI MOHD ZAILANI"
+    fullName: "ARIESYAA ARYANA BINTI MOHD ZAILANI",
+    shortName: "Ariesyaa Aryana"
   },
 
-  // MAJLIS
+  dateText: "19 Disember 2026",
+  dateShort: "19.12.2026",
   day: "Sabtu",
-  date: "19 Disember 2026",
-  dateShort: "19 | 12 | 2026",
 
-  akadTime: "9.00 PAGI",
-  receptionTime: "12.00 TENGAH HARI – 3.00 PETANG",
+  akadTime: "9.00 pagi",
+  receptionTime: "12.00 tengah hari - 3.00 petang",
 
-  countdown: "2026-12-19T09:00:00",
+  countdownDate: "2026-12-19T09:00:00",
 
-  // TEMPAT
   venue: "IMAN IRIS GRAND HALL",
-  location: "KOTA DAMANSARA, SELANGOR",
-  maps: "https://maps.app.goo.gl/CvdHNXxJkoVAETxf9",
-
-  // IBU BAPA
-  groomParents: {
-    father: "NAZRI BIN SABRI",
-    mother: "HALIJAH BINTI MAT SUJAK"
-  },
-
-  brideParents: {
-    father: "MOHD ZAILANI BIN KARI",
-    mother: "AAINA AZURA BINTI ROSLI"
-  },
-
-  // CONTACT
-  contacts: {
-    groom: [
-      {
-        name: "Haiqal",
-        phone: "601168413075",
-        display: "011-68413075"
-      },
-      {
-        name: "Ibu Haiqal",
-        phone: "60183154970",
-        display: "018-3154970"
-      },
-      {
-        name: "Abah Haiqal",
-        phone: "60183740280",
-        display: "018-3740280"
-      }
-    ],
-
-    bride: [
-      {
-        name: "Icha",
-        phone: "60148740284",
-        display: "014-8740284"
-      },
-      {
-        name: "Ibu Icha",
-        phone: "60172564550",
-        display: "017-2564550"
-      },
-      {
-        name: "Bapa Icha",
-        phone: "60187720015",
-        display: "018-7720015"
-      }
-    ]
-  },
-
-  // DRESS CODE
-  dressCode: {
-    groomFamily: "CHAMPAGNE",
-    brideFamily: "PINK"
-  }
+  maps: "https://maps.app.goo.gl/CvdHNXxJkoVAETxf9"
 };
