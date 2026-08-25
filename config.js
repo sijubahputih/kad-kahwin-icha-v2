4e043d5a9296f6dd4ccea2304aa827e0e0bd6d68
sijubahputih <kimie.ummah@gmail.com>
2026-08-26 02:20:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,83 @@
+const wedding = {
+  // PENGANTIN
+  groom: {
+    first: "NOR ALIF HAIQAL",
+    last: "BIN MOHD NAZRI"
+  },
 
+  bride: {
+    first: "ARIESYAA ARYANA",
+    last: "BINTI MOHD ZAILANI"
+  },
+
+  // MAJLIS
+  day: "Sabtu",
+  date: "19 Disember 2026",
+  dateShort: "19 | 12 | 2026",
+
+  akadTime: "9.00 PAGI",
+  receptionTime: "12.00 TENGAH HARI – 3.00 PETANG",
+
+  countdown: "2026-12-19T09:00:00",
+
+  // TEMPAT
+  venue: "IMAN IRIS GRAND HALL",
+  location: "KOTA DAMANSARA, SELANGOR",
+  maps: "https://maps.app.goo.gl/CvdHNXxJkoVAETxf9",
+
+  // IBU BAPA
+  groomParents: {
+    father: "NAZRI BIN SABRI",
+    mother: "HALIJAH BINTI MAT SUJAK"
+  },
+
+  brideParents: {
+    father: "MOHD ZAILANI BIN KARI",
+    mother: "AAINA AZURA BINTI ROSLI"
+  },
+
+  // CONTACT
+  contacts: {
+    groom: [
+      {
+        name: "Haiqal",
+        phone: "601168413075",
+        display: "011-68413075"
+      },
+      {
+        name: "Ibu Haiqal",
+        phone: "60183154970",
+        display: "018-3154970"
+      },
+      {
+        name: "Abah Haiqal",
+        phone: "60183740280",
+        display: "018-3740280"
+      }
+    ],
+
+    bride: [
+      {
+        name: "Icha",
+        phone: "60148740284",
+        display: "014-8740284"
+      },
+      {
+        name: "Ibu Icha",
+        phone: "60172564550",
+        display: "017-2564550"
+      },
+      {
+        name: "Bapa Icha",
+        phone: "60187720015",
+        display: "018-7720015"
+      }
+    ]
+  },
+
+  // DRESS CODE
+  dressCode: {
+    groomFamily: "CHAMPAGNE",
+    brideFamily: "PINK"
+  }
+};
