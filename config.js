212670f41e38de6a28133d5023445b7440e991d3
sijubahputih <kimie.ummah@gmail.com>
2026-08-28 00:47:06 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+const wedding = {
+  groom: {
+    fullName: "NOR ALIF HAIQAL BIN MOHD NAZRI",
+    shortName: "Nor Alif Haiqal"
+  },
+
+  bride: {
+    fullName: "ARIESYAA ARYANA BINTI MOHD ZAILANI",
+    shortName: "Ariesyaa Aryana"
+  },
+
+  dateText: "19 Disember 2026",
+  dateShort: "19.12.2026",
+  day: "Sabtu",
+
+  akadTime: "9.00 pagi",
+  receptionTime: "12.00 tengah hari - 3.00 petang",
+
+  countdownDate: "2026-12-19T09:00:00",
+
+  venue: "IMAN IRIS GRAND HALL",
+  maps: "https://maps.app.goo.gl/CvdHNXxJkoVAETxf9"
+};
